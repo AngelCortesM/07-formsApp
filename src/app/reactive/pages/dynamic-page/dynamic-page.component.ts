bd4fd9205dbb947ee042a730fc666819241e6997
{AngelCortesM} <joseangelcortesmontoya@gmail.com>
2024-12-19 10:23:14 -0500

    Agregar funcionalidad para añadir juegos favoritos y mejorar la validación del formulario

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   standalone: false,
@@ -17,6 +23,7 @@ export class DynamicPageComponent {
   // }
 
   public myForm: FormGroup;
+  public newFavorite: FormControl = new FormControl('', Validators.required);
 
   constructor(private fb: FormBuilder) {
     this.myForm = this.fb.group({
@@ -51,6 +58,14 @@ export class DynamicPageComponent {
     return null;
   }
 
+  onAddToFavorites(): void {
+    if (this.newFavorite.invalid) return;
+    const newGame = this.newFavorite.value;
+    //this.favoriteGames.push(new FormControl(newGame, Validators.required));
+    this.favoriteGames.push(this.fb.control(newGame, Validators.required));
+    this.newFavorite.reset();
+  }
+
   isValidFieldInArray(formArray: FormArray, index: number) {
     return (
       formArray.controls[index].errors && formArray.controls[index].touched
@@ -66,6 +81,7 @@ export class DynamicPageComponent {
       return;
     }
     console.log(this.myForm.value);
+    (this.myForm.controls['favoriteGames'] as FormArray) = this.fb.array([]);
     this.myForm.reset();
   }
 }
